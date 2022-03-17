f239d738e3c884053fe052b32e058092ba5b320f
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2022-03-17 15:06:43 +0100

    add section

--- src/components/Section_6.tsx
+++ src/components/Section_6.tsx
@@ -17,8 +17,11 @@ export default Section_6;
 const Section_6Element = styled.div`
        background: rgb(118,77,128);  
       background: linear-gradient(21deg, rgba(118,77,128,1) 13%, rgba(115,80,126,1) 49%, rgba(48,50,50,1) 74%); 
-      /* height:80vh; */
 
 
+
+      h2{
+        
+      }
 
 `
